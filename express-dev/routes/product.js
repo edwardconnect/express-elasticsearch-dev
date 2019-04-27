8d4cd7b49ccd62d531d81327aca2d0935b0fb337
Edward <elchu@connect.ust.hk>
2019-04-27 23:56:41 +0800

    Update product route

--- express-dev/routes/product.js
+++ express-dev/routes/product.js
@@ -1,57 +1,61 @@
 import express from 'express';
 import mongooseClient from '../src/db/mongoose';
 import { Product } from '../src/model/product-model';
-import { saveProduct, esClient } from '../src/db/elasticsearch'
+import { ProductSearchRepository } from "../src/db/elasticsearch-repository/product-search-repository";
 
 const router = express.Router();
+const productSearchRepository = new ProductSearchRepository();
 
 router.post('/', async (req, res, next) => {
+    console.log('REST request to create product')
+    console.log(JSON.stringify(req.body), undefined, 2);
+
     const product = new Product(req.body);
     try {
-        const mongoProduct = await product.save();
-        const elasticProduct = await saveProduct(mongoProduct);
-        res.send();
-    } catch (error) {
-        console.log('Error!', error)
-        next();
+        const mongoResult = await product.save();
+        const elasticResult = await productSearchRepository.addProductDocument(mongoResult);
+        res.send(mongoResult);
+    } catch(error) {
+        next(error);
     }
-    product.save()
-        .then((savedProduct) => {
-            console.log(savedProduct)
-            res.send(savedProduct)
-        })
-        .catch(error => {
-            console.log('Error', error)
-        })
 })
 
 router.get('/elasticsearch', (req, res, next) => {
-    console.log('REST request to search product');
-    const queryString = req.query.queryString;
-    const size = req.query.size
-w
-    const query = {
-        query: {
-            prefix: { name: queryString }
-        }
+    console.log('REST request to get product');
+    const queryName = req.query.name;
+
+    if (queryName === undefined) {
+        next('Request param "name" not found');
+    } 
+
+    try {
+        const result = await productSearchRepository.findProductByName(queryName);
+        res.send(result);
+    } catch(error) {
+        next(error)
     }
-
-    esClient.search({
-        index: 'product',
-        q: 'name:Bo'
-    }).then((result) => {
-        console.log(result);
-        res.send(result.body)
-    }).catch(error => {
-        console.log('Error',error);
-        res.send();
-    })
-
-    // console.log(queryString)
-    // console.log(size)
-    // console.log(req.query)
-    // res.send();
 })
+
+// router.post('/', async (req, res, next) => {
+//     const product = new Product(req.body);
+//     try {
+//         const mongoProduct = await product.save();
+//         const elasticProduct = await saveProduct(mongoProduct);
+//         res.send();
+//     } catch (error) {
+//         console.log('Error!', error)
+//         next();
+//     }
+//     product.save()
+//         .then((savedProduct) => {
+//             console.log(savedProduct)
+//             res.send(savedProduct)
+//         })
+//         .catch(error => {
+//             console.log('Error', error)
+//         })
+// })
+
 
 router.get('/', (req, res, next) => {
     console.log('REST request to get product');
