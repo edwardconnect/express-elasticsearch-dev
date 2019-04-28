bf752a23308da8f577856cd145c7fde64e76991b
Edward <elchu@connect.ust.hk>
2019-04-28 22:51:29 +0800

    Install cors

--- express-dev/app.js
+++ express-dev/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -29,6 +31,12 @@ app.use('/products', productRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
