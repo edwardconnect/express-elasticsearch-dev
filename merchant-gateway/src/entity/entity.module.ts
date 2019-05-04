a635bdb11a97f52795eafe0ac4da3c70529b6dc0
Edward <elchu@connect.ust.hk>
2019-05-04 11:23:20 +0800

    Set up module settings

--- merchant-gateway/src/entity/entity.module.ts
+++ merchant-gateway/src/entity/entity.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommodityModule } from './commodity/commodity.module';
+import { SettingModule } from './setting/setting.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    CommodityModule,
+    SettingModule,
+    UserModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class EntityModule { }
--- merchant-gateway/src/layout/layout.module.ts
+++ merchant-gateway/src/layout/layout.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { MainComponent } from './main/main.component';
+import { SharedCommonModule } from 'shared/shared-common/shared-common.module';
 
 @NgModule({
   declarations: [NavbarComponent, SidenavComponent, MainComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedCommonModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LayoutModule { }
