d3590886d0638bbaff3a4ec66a035b4934b1e1ac
Edward <elchu@connect.ust.hk>
2019-05-26 18:02:36 +0800

    Add updating commodity function in commodity service

--- merchant-gateway/src/entity/commodity/commodity.service.ts
+++ merchant-gateway/src/entity/commodity/commodity.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpResponse, HttpClient } from '@angular/common/http';
 import { Commodity } from './commodity.model';
+import { createRequestOption } from 'src/shared/utils/request-util';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,24 @@ export class CommodityService {
   private resourceUrl = 'http://localhost:80/products'
   constructor(private http: HttpClient) { }
 
+  /**
+   * GET /products - Get the product by request params
+   * @param req request params
+   */
   getCommodities(req): Observable<HttpResponse<Commodity[]>> {
-    
-    return this.http.get<Commodity[]>(this.resourceUrl, {observe: 'response'})
+    const options = createRequestOption(req);
+    return this.http.get<Commodity[]>(this.resourceUrl, { params: options, observe: 'response' })
   }
 
+  /**
+   * POST /products - Create the product
+   * @param commodity the request commodity to be created
+   */
   createCommodity(commodity: Commodity): Observable<HttpResponse<Commodity>> {
-    return this.http.post<Commodity>(this.resourceUrl, commodity,{observe: 'response'});
+    return this.http.post<Commodity>(this.resourceUrl, commodity, { observe: 'response' });
+  }
+
+  updateCommodity(commodity: Commodity): Observable<HttpResponse<Commodity>> {
+    return this.http.put<Commodity>(this.resourceUrl, commodity, { observe: 'response' });
   }
 }
